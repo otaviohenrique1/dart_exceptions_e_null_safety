fea4c689e768f8a69d3baa5d61555564d3804789
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-02-20 20:16:28 -0300

    commit 5

--- bin/dart_exceptions_e_null_safety.dart
+++ bin/dart_exceptions_e_null_safety.dart
@@ -1,3 +1,5 @@
+// import 'dart:io';
+
 void main(List<String> arguments) {
   print("Started main");
   functionOne();
@@ -8,9 +10,56 @@ void functionOne() {
   print("Started F01");
   try {
     functionTwo();
-  } on FormatException {
+  } /* on FormatException */ catch (exception, stackTrace) {
     print("A conversão não pode ser faita.");
+    print("-----------------------------");
+    // print(exception.toString());
+    // print(exception.runtimeType);
+    print(exception);
+    print("-----------------------------");
+    print(stackTrace.toString());
+    /* stackTrace => pilha de informacoes de erros */
+    // rethrow;
+    /* rethrow => Lança a exceção para o próximo quadro da Pilha de Execução, mesmo que a exceção seja tratada com o try */
+  } finally {
+    print("Chegou no finally");
   }
+
+  /* 
+    try {
+      functionTwo();
+    } catch (exception, stackTrace) {
+      print("A conversão não pode ser faita.");
+      // print(exception.toString());
+      // print(exception.runtimeType);
+      print(exception);
+      print("-----------------------------");
+      print(stackTrace.toString());
+      /* stackTrace => pilha de informacoes de erros */
+    } finally {
+      print("Chegou no finally");
+    }
+  */
+
+  /* 
+    try {
+      functionTwo();
+    } on FormatException catch (e) {
+      print("A conversão não pode ser faita.");
+      print(e.message);
+      print(e.source);
+      print(e.toString());
+    } on HttpException catch (e) {
+      print("Uma HttpException foi encontrada.");
+      print(e.toString());
+    } on IOException catch (e) {
+      print("Uma IOException foi encontrada.");
+      print(e.toString());
+    } on Exception catch (e) {
+      print("Uma Exception foi encontrada.");
+      print(e.toString());
+    }
+  */
   print("Finished F01");
 }
 
